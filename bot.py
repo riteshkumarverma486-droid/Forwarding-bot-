62661777442a82c49dad426316214bf79eb91dd6
riteshkumarverma486-droid <riteshkumarverma486@gmail.com>
2026-04-27 23:39:06 +0530

    Refactor bot to use new Client instance and remove Flask

--- bot.py
+++ bot.py
@@ -1,42 +1,12 @@
-import os
-import threading
 from pyrogram import Client
-from flask import Flask
+from config import API_ID, API_HASH, BOT_TOKEN
 
-# Flask app (dummy server for Render)
-app_flask = Flask(__name__)
-
-@app_flask.route("/")
-def home():
-    return "Bot is running!"
-
-def run_web():
-    port = int(os.environ.get("PORT", 10000))
-    app_flask.run(host="0.0.0.0", port=port)
-
-# Telegram Bot
-API_ID = os.getenv("API_ID")
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-if not API_ID or not API_HASH or not BOT_TOKEN:
-    raise Exception("❌ Missing ENV variables")
-
-API_ID = int(API_ID)
-
-print("✅ ENV Loaded")
-
-bot = Client(
-    "advanced-search-bot",
+app = Client(
+    "pro-bot",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
     plugins=dict(root="handlers")
 )
 
-# Run Flask in separate thread
-threading.Thread(target=run_web).start()
-
-print("🚀 Bot + Web running...")
-
-bot.run()
+app.run()
