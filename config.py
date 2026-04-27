e4445f0d0abe88e81b3eb23992c82b8105d2c8aa
riteshkumarverma486-droid <riteshkumarverma486@gmail.com>
2026-04-27 23:32:38 +0530

    Update FORCE_JOIN comment to reflect backup channel

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-FORCE_JOIN = os.getenv("FORCE_JOIN")  # channel username
+FORCE_JOIN = os.getenv("FORCE_JOIN")  # Backup_Channel42
