3e86d512f9e498dff930c2948eb2cb465354837a
riteshkumarverma486-droid <riteshkumarverma486@gmail.com>
2026-04-28 09:55:24 +0530

    Refactor database.py to manage connections
    Updated database schema to create connections table and removed force channel functions.

--- database.py
+++ database.py
@@ -1,17 +1,14 @@
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS force_channels (
-    channel TEXT
+import sqlite3
+
+conn = sqlite3.connect("bot.db", check_same_thread=False)
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS connections(
+  user_id INTEGER,
+  group_id INTEGER,
+  channel_id INTEGER
 )
 """)
 
-def add_force_channel(channel):
-    cursor.execute("INSERT INTO force_channels VALUES (?)", (channel,))
-    conn.commit()
-
-def remove_force_channel(channel):
-    cursor.execute("DELETE FROM force_channels WHERE channel=?", (channel,))
-    conn.commit()
-
-def get_force_channels():
-    cursor.execute("SELECT channel FROM force_channels")
-    return [x[0] for x in cursor.fetchall()]
+conn.commit()
