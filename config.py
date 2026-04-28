46c6f0e6a0fb20116a4286e54729215cc077caaa
riteshkumarverma486-droid <riteshkumarverma486@gmail.com>
2026-04-28 08:04:22 +0530

    Update FORCE_JOIN and add ADMINS configuration

--- config.py
+++ config.py
@@ -4,4 +4,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-FORCE_JOIN = os.getenv("FORCE_JOIN")  # Backup_Channel42
+# comma separated usernames without @
+FORCE_JOIN = os.getenv("FORCE_JOIN", "")  # e.g. Backup_Channel42,Channel2
+
+ADMINS = [int(x) for x in os.getenv("ADMINS", "").split(",") if x]
