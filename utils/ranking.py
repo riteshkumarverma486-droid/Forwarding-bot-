ca4c8334e227e628859adcb853be19d56ad4f332
riteshkumarverma486-droid <riteshkumarverma486@gmail.com>
2026-04-28 22:18:06 +0530

    Delete utils directory

--- utils/ranking.py
+++
@@ -1,8 +0,0 @@
-def score(name, query):
-    name = (name or "").lower()
-    q = query.lower()
-    s = 0
-    if q in name: s += 5
-    if name.startswith(q): s += 3
-    s -= abs(len(name) - len(q)) * 0.01
-    return s
