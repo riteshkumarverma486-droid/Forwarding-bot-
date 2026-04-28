961645cb284cdc3892e5f2e1b346ed946d57dcc7
riteshkumarverma486-droid <riteshkumarverma486@gmail.com>
2026-04-28 08:14:44 +0530

    Refactor start command handler for simplified response

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,5 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
 
 @Client.on_message(filters.command("start"))
-async def start(client, message: Message):
-    await message.reply(
-        "👋 Welcome!\n\n"
-        "Use /connect to link your channel & group\n"
-        "Then just type anything in group to search 🔍"
-    )
+async def start(_, m):
+    await m.reply("👋 Welcome!\nGroup me search karo 🔍")
