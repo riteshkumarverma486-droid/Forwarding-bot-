985eac9458d20e710eb980c3f29a7b5111f152ad
riteshkumarverma486-droid <riteshkumarverma486@gmail.com>
2026-04-28 10:47:36 +0530

    Implement force channel management functions
    Added functions to manage force channels in the database.

--- database.py
+++ database.py
@@ -3,12 +3,25 @@ import sqlite3
 conn = sqlite3.connect("bot.db", check_same_thread=False)
 cur = conn.cursor()
 
+# table
 cur.execute("""
-CREATE TABLE IF NOT EXISTS connections(
-  user_id INTEGER,
-  group_id INTEGER,
-  channel_id INTEGER
+CREATE TABLE IF NOT EXISTS force_channels(
+    channel_id TEXT
 )
 """)
-
 conn.commit()
+
+# ✅ ADD
+def add_force_channel(channel):
+    cur.execute("INSERT INTO force_channels VALUES (?)", (channel,))
+    conn.commit()
+
+# ✅ REMOVE
+def remove_force_channel(channel):
+    cur.execute("DELETE FROM force_channels WHERE channel_id = ?", (channel,))
+    conn.commit()
+
+# ✅ GET
+def get_force_channels():
+    cur.execute("SELECT channel_id FROM force_channels")
+    return [i[0] for i in cur.fetchall()]
