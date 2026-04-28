d3873786eac1d2c8c80449c964ac36a1bda648f4
riteshkumarverma486-droid <riteshkumarverma486@gmail.com>
2026-04-28 08:06:08 +0530

    Refactor cache to include TTL for entries
    Implement time-based cache expiration for queries.

--- utils/cache.py
+++ utils/cache.py
@@ -1,7 +1,18 @@
-cache = {}
+from time import time
 
-def get_cache(query):
-    return cache.get(query)
+_cache = {}  # {query: (timestamp, (channel_id, msg_id))}
 
-def set_cache(query, data):
-    cache[query] = data
+TTL = 60 * 5  # 5 min
+
+def get_cache(q):
+    v = _cache.get(q)
+    if not v:
+        return None
+    ts, data = v
+    if time() - ts > TTL:
+        _cache.pop(q, None)
+        return None
+    return data
+
+def set_cache(q, data):
+    _cache[q] = (time(), data)
